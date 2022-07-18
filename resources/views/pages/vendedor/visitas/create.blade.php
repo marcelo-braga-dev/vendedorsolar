fbf6fb65a8375a4561791be0361d313bbf6ea1c9
Marcelo Braga <marcelobraga.dev@gmail.com>
2022-07-17 21:06:36 -0300

    Atualizacao

--- resources/views/pages/vendedor/visitas/create.blade.php
+++ resources/views/pages/vendedor/visitas/create.blade.php
@@ -11,7 +11,7 @@
                     </x-inputs.select>
                 </div>
                 <div class="col-6 col-md-3">
-                    <x-inputs.datetime label="Data/Hora" name="data" required></x-inputs.datetime>
+                    <x-inputs.date-time label="Data/Hora" name="data" required></x-inputs.date-time>
                 </div>
             </div>
             <div class="form-row">
