bbb21f40a44fee02698c7d2b07ed55b4c7376a25
Marcelo Braga <marcelobraga.dev@gmail.com>
2022-07-18 20:12:01 -0300

    Atualizacao

--- routes/db/clientes.php
+++ routes/db/clientes.php
@@ -6,69 +6,72 @@ function atualizarClientes()
 {
     $vendedores = (new \App\Models\User())->newQuery()->get();
 
+    $mysql = conectaTabela();
     foreach ($vendedores as $vendedor) {
-        $mysql = conectaTabela();
+        try {
+            $resultado = $mysql->query("SELECT * FROM `{$vendedor->id}` ORDER BY `id` DESC");
+            $dados = $resultado->fetch_all(MYSQLI_ASSOC);
 
-        $resultado = $mysql->query("SELECT * FROM `{$vendedor->id}` ORDER BY `id` DESC");
-        $dados = $resultado->fetch_all(MYSQLI_ASSOC);
+            foreach ($dados as $dado) {
+                $nome = $dado['nome'];
+                if (empty($nome)) $nome = $dado['razao_social'];
 
-        foreach ($dados as $dado) {
-            $nome = $dado['nome'];
-            if (empty($nome)) $nome = $dado['razao_social'];
+                $cliente = (new \App\Models\Clientes())->newQuery()
+                    ->create([
+                        'nome' => $nome,
+                        'users_id' => $vendedor->id,
+                        'cidades_estados_id' => getIdLocalidadeIntegracao($dado['cidade'], $dado['estado']),
+                        'status' => 'novo'
+                    ]);
 
-            $cliente = (new \App\Models\Clientes())->newQuery()
-                ->create([
-                    'nome' => $nome,
-                    'users_id' => $vendedor->id,
-                    'cidades_estados_id' => getIdLocalidadeIntegracao($dado['cidade'], $dado['estado']),
-                    'status' => 'novo'
-                ]);
+                $meta = (new \App\Models\ClientesMetas())->newQuery();
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'cpf', 'value' => $dado['cpf']]);
 
-            $meta = (new \App\Models\ClientesMetas())->newQuery();
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'cpf', 'value' => $dado['cpf']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'rg', 'value' => $dado['rg']]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'rg', 'value' => $dado['rg']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'cnpj', 'value' => $dado['cnpj']]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'cnpj', 'value' => $dado['cnpj']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'razao_social', 'value' => $dado['razao_social']]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'razao_social', 'value' => $dado['razao_social']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'inscricao_social', 'value' => $dado['inscricao_estadual']]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'inscricao_social', 'value' => $dado['inscricao_estadual']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'nome_fantasia', 'value' => $dado['nome_fantasia']]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'nome_fantasia', 'value' => $dado['nome_fantasia']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'celular', 'value' => $dado['celular']]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'celular', 'value' => $dado['celular']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'telefone', 'value' => $dado['telefone']]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'telefone', 'value' => $dado['telefone']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'email', 'value' => $dado['email']]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'email', 'value' => $dado['email']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'cep', 'value' => $dado['cep']]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'cep', 'value' => $dado['cep']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'endereco', 'value' => $dado['endereco']]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'endereco', 'value' => $dado['endereco']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'complemento', 'value' => null]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'complemento', 'value' => null]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'numero', 'value' => $dado['numero']]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'numero', 'value' => $dado['numero']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'bairro', 'value' => $dado['bairro']]);
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'bairro', 'value' => $dado['bairro']]);
+                $meta->create(['cliente_id' => $cliente->id,
+                    'meta' => 'whatsapp', 'value' => $dado['whatsapp']]);
+            }
+        } catch (Error $exception) {
 
-            $meta->create(['cliente_id' => $cliente->id,
-                'meta' => 'whatsapp', 'value' => $dado['whatsapp']]);
         }
     }
 }
