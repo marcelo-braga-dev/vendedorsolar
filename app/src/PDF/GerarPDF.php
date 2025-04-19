153216244cdcbceb23658bc881ffc56add0e9f56
MARCELO <marcelobraga.dev@gmail.com>
2025-04-19 19:51:19 -0300

    update generate PDF

---
+++ app/src/PDF/GerarPDF.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\src\PDF;
+
+use App\src\PDF\Solmar\Construtor;
+
+class GerarPDF
+{
+    public function gerar($dados)
+    {
+//        $gerar = (new Construtor($dados->id, $dados->grafico_geracao, $dados->grafico_payback));
+        $gerar = (new Construtor(1, '', ''));
+
+        //        $gerar = (new Construtor($dados->id, $dados->grafico_geracao, $dados->grafico_payback));
+        $gerar = (new Construtor(1, '', ''));
+        return $gerar->gerar();
+    }
+}
