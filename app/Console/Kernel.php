d77c08ed7d61b08f4c10b03e4500090166b739b7
MARCELO <marcelobraga.dev@gmail.com>
2025-04-20 17:26:47 -0300

    teste

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('app:integracao-edeltec')->dailyAt('04:00');
+//        $schedule->command('app:integracao-edeltec')->dailyAt('04:00');
+        $schedule->command('app:integracao-edeltec')->dailyAt('17:20');
     }
 
     /**
