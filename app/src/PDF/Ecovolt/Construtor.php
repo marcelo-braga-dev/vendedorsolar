35f45a7e436afaa1acb3e607b8f7418bd5495971
MARCELO <marcelobraga.dev@gmail.com>
2025-09-04 00:04:56 -0300

    update

--- app/src/PDF/Ecovolt/Construtor.php
+++ app/src/PDF/Ecovolt/Construtor.php
@@ -30,6 +30,7 @@ class Construtor extends DadosOrcamento
 
         try {
             $this->mpdf = new Mpdf([
+                'tempDir' => storage_path('app/tmp'),
                 "format" => "A4",
                 'margin_top' => 0,
                 'margin_bottom' => 0,
